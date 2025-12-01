21701510581671139abd8d0fb35253844ca326dc
nbreval <nicolasbrevalrodriguez@gmail.com>
2025-12-01 19:17:08 +0100

    chore: add some docstrings

--- apps/gateway/src/main/java/org/nbreval/weather_twin/gateway/shared/MeanMeasureEvent.java
+++ apps/gateway/src/main/java/org/nbreval/weather_twin/gateway/shared/MeanMeasureEvent.java
@@ -1,6 +1,12 @@
 package org.nbreval.weather_twin.gateway.shared;
 
+/**
+ * Event representing the mean measure over a specified time window from a sensor.
+ */
 public class MeanMeasureEvent extends SensorNotification {
+    /**
+     * Size of the time window (in milliseconds) over which the mean measure is calculated.
+     */
     private final long windowSize;
 
     public MeanMeasureEvent(Object source, String deviceId, String sensorId, long utcTimestamp, double measure, long windowSize) {
@@ -8,6 +14,10 @@ public class MeanMeasureEvent extends SensorNotification {
         this.windowSize = windowSize;
     }
 
+    /**
+     * Gets the size of the time window.
+     * @return the window size in milliseconds
+     */
     public long getWindowSize() {
         return windowSize;
     }
