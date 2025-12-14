5a772893ae67fe5519e3dcc6849d5eca95c6196d
nbreval <nicolasbrevalrodriguez@gmail.com>
2025-12-14 23:22:44 +0100

    fix: initialize CoapAdapter and create database folders if not exist

--- apps/gateway/src/main/java/org/nbreval/weather_twin/gateway/infrastructure/config/InitializationConfig.java
+++ apps/gateway/src/main/java/org/nbreval/weather_twin/gateway/infrastructure/config/InitializationConfig.java
@@ -1,19 +1,26 @@
 package org.nbreval.weather_twin.gateway.infrastructure.config;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Set;
 
 import org.nbreval.weather_twin.gateway.application.port.in.DispatcherPort;
 import org.nbreval.weather_twin.gateway.application.port.in.SchedulerPort;
+import org.nbreval.weather_twin.gateway.application.port.in.SensorConfigurationPort;
 import org.nbreval.weather_twin.gateway.application.port.in.WTALLogicPort;
 import org.nbreval.weather_twin.gateway.application.port.out.AggregationsDbPort;
 import org.nbreval.weather_twin.gateway.application.port.out.ExpressionsDbPort;
 import org.nbreval.weather_twin.gateway.application.port.out.MeasureProcessorPort;
 import org.nbreval.weather_twin.gateway.application.service.DispatcherService;
 import org.nbreval.weather_twin.gateway.application.service.SchedulerService;
+import org.nbreval.weather_twin.gateway.application.service.SensorConfigurationService;
 import org.nbreval.weather_twin.gateway.domain.service.WtalLogicService;
 import org.nbreval.weather_twin.gateway.infrastructure.adapter.AggregationsDbAdapter;
+import org.nbreval.weather_twin.gateway.infrastructure.adapter.CoapAdapter;
 import org.nbreval.weather_twin.gateway.infrastructure.adapter.ExpressionsDbAdapter;
 import org.nbreval.weather_twin.gateway.infrastructure.adapter.MeasureProcessorAdapter;
+import org.nbreval.weather_twin.gateway.infrastructure.exception.GatewayInitializationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,12 +32,46 @@ public class InitializationConfig {
   public ExpressionsDbPort expressionsDB(
       @Value("${db.aggregators.location}") String aggregatorsLocation,
       @Value("${db.flushes.location}") String flushesLocation) {
+    var aggregatorsParentFolder = Paths.get(aggregatorsLocation).getParent();
+
+    if (!Files.exists(aggregatorsParentFolder)) {
+      try {
+        Files.createDirectories(aggregatorsParentFolder);
+      } catch (IOException e) {
+        throw new GatewayInitializationException(
+            "Error creating parent folder for aggregators location '%s'".formatted(aggregatorsLocation));
+      }
+    }
+
+    var flushesParentFolder = Paths.get(flushesLocation).getParent();
+
+    if (!Files.exists(flushesParentFolder)) {
+      try {
+        Files.createDirectories(flushesParentFolder);
+      } catch (IOException e) {
+        throw new GatewayInitializationException(
+            "Error creating parent folder for flushes location '%s'".formatted(flushesLocation));
+      }
+    }
+
     return new ExpressionsDbAdapter(aggregatorsLocation, flushesLocation);
   }
 
   @Bean
   public AggregationsDbPort aggregationsDB(
       @Value("${db.aggregations.location}") String aggregationsLocation) {
+
+    var aggregationsParentFolder = Paths.get(aggregationsLocation).getParent();
+
+    if (!Files.exists(aggregationsParentFolder)) {
+      try {
+        Files.createDirectories(aggregationsParentFolder);
+      } catch (IOException e) {
+        throw new GatewayInitializationException(
+            "Error creating parent folder for aggregations location '%s'".formatted(aggregationsLocation));
+      }
+    }
+
     var aggregationsDB = new AggregationsDbAdapter(aggregationsLocation);
 
     // Clean previous aggregations when bean is created
@@ -65,6 +106,11 @@ public class InitializationConfig {
   }
 
   @Bean
+  public CoapAdapter coapAdapter(DispatcherPort dispatcher) {
+    return new CoapAdapter(dispatcher);
+  }
+
+  @Bean
   public SchedulerPort scheduler(AggregationsDbPort aggregationsDB, ExpressionsDbPort expressionsDB,
       MeasureProcessorPort measureProcessor) {
     // TODO: Output connectors
@@ -73,5 +119,11 @@ public class InitializationConfig {
     aggregationsDB.getAllIntervals().forEach(interval -> scheduler.schedule(interval));
 
     return scheduler;
+  }
+
+  @Bean
+  public SensorConfigurationPort sensorConfiguration(AggregationsDbPort aggregationsDB,
+      ExpressionsDbPort expressionsDB) {
+    return new SensorConfigurationService(aggregationsDB, expressionsDB);
   }
 }
