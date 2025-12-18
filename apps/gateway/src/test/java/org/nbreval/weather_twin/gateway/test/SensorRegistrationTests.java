3ae1b03896ab028fc5258c30646fe84f612c081d
nbreval <nicolasbrevalrodriguez@gmail.com>
2025-12-18 11:09:40 +0100

    chore: remove unused import

--- apps/gateway/src/test/java/org/nbreval/weather_twin/gateway/test/SensorRegistrationTests.java
+++ apps/gateway/src/test/java/org/nbreval/weather_twin/gateway/test/SensorRegistrationTests.java
@@ -1,7 +1,6 @@
 package org.nbreval.weather_twin.gateway.test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Set;
 
